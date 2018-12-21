ae23f5ec066b0a5200c1506a85d20a93abe95e2b
Milos <stankomanster@gmail.com>
2018-12-21 22:47:04 +0100

    Automapper setup
    added automapper to startup.cs 
    added profile for LoginUser

---
+++ SuperA/Models/Maps/LoginUserMap.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using SuperA.Core;
+
+namespace SuperA.Models.Maps
+{
+    public class LoginUserMap : Profile
+    {
+        public LoginUserMap()
+        {
+            CreateMap<User, LoginModel>();
+        }
+    }
+}
